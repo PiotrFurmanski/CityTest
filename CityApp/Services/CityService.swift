8077e690e2c00e219c403de8b31045d6476e52fb
Piotr Furmanski <piotr.furmanski@yahoo.com>
2020-07-27 18:32:45 +0200

    new endpoint

--- CityApp/Services/CityService.swift
+++ CityApp/Services/CityService.swift
@@ -22,7 +22,7 @@ protocol CityServiceProtocol: AnyObject {
 class CityService: CityServiceProtocol {
     private struct Constanst {
         static let baseUrl = "https://gist.githubusercontent.com/PiotrFurmanski/"
-        static let cityListEndpoint = "115dd75791870216934e3d27e919367f/raw/bf7ad481aed63753d285f5cf921e0a49bc876cb4/cityList"
+        static let cityListEndpoint = "115dd75791870216934e3d27e919367f/raw/d8bddbe0a5eda0ff81893973e239f411978c73d4/cityList"
     }
 
     func getCityList(completion: @escaping (Result<[CityModel], Error>) -> Void) {
